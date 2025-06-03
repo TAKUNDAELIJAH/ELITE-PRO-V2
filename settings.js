fa1d999f493859c5cc591ef5e6a40add562b7116
TAKUNDAELIJAH <168351848+TAKUNDAELIJAH@users.noreply.github.com>
2025-06-03 21:13:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,30 +3,30 @@ const chalk = require('chalk')
 
 //BOT SETTINGS//
 global.xprefix = '.'
-global.ownername = 'ElitePro'
+global.ownername = 'King Eli'
 global.botname = 'ELITE-PRO-V2'
-global.ytname = "YT: EliteProTechs"
+global.ytname = "YT: Cyberking"
 global.socialm = "GitHub: EliteProTech"
-global.location = "Port Harcourt, Nigeria"
-global.ownernumber = '2348109263390'
+global.location = "Mutare, Zimbabwe"
+global.ownernumber = '263781265449'
 
 //BOT SETTINGS 2//
-global.premium = ["2348109263390"] //Premium 
+global.premium = ["263781265449"] //Premium 
 global.websitex = "https://youtube.com/@eliteprotechs"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
 global.wm = "ELITE-PRO-V2"
 global.botscript = 'https://github.com/EliteProTech/Elite-Pro-V2'
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2\n\n+2348109263390"
-global.creator = "2348109263390@s.whatsapp.net"
+global.auth = "Taku \n\n+263781265449"
+global.creator = "263781265449"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg'
 
 //DEFAULT SETTINGS//
 global.hituet = 0
 global.typemenu = 'v1' // v1, v2 too v9
 global.typereply = 'v2' // v1, v2 and v3
-global.autoblocknumber = '92' //autoblock country
+global.autoblocknumber = '263' //autoblock country
 global.antiforeignnumber = '91' //anti foreign number
 // TOGGLE CMDS: Enable=true Disable=false
 global.welcome = false //welcome/left in groups
